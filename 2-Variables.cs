d6e67409c6e14da720a2a891b06fe53e14e9d8d9
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-16 23:14:24 +0300

    Update 2-Variables.cs

--- 2-Variables.cs
+++ 2-Variables.cs
@@ -5,17 +5,28 @@ namespace CSharpExamples
         static void main(string[] args)
         {
             string _string = "This is a string";
+            System.Console.WriteLine(_string);
             char _char1 = 'a';
+            System.Console.WriteLine(_char1);
             //char _char2 = 57;
             float _float = 15.235f;
+            System.Console.WriteLine(_float);
             double _double = 1.42363463;
+            System.Console.WriteLine(_double);
             var _var1 = "This is a variable";
+            System.Console.WriteLine(_var1);
             var _var2 = 'b';
+            System.Console.WriteLine(_var2);
             var _var3 = 16.235f;
+            System.Console.WriteLine(_var3);
             var _var4 = 2.42363463;
+            System.Console.WriteLine(_var4);
             byte _byte = 0x10;
+            System.Console.WriteLine(_byte);
             object _object = _float;
+            System.Console.WriteLine(_object);
             float _float2 = (float)_object;
+            System.Console.WriteLine(_float2);
             
             
             /*Output:
