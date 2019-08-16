5cbf0d1fc902ab6161efe7cf35ae2cadf9063634
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-16 22:54:22 +0300

    Update 1-Hello-World.cs

--- 1-Hello-World.cs
+++ 1-Hello-World.cs
@@ -1,12 +1,12 @@
 using System;
 
-namespace Sişarpdeneme
+namespace CSharpExamples
 {
     class Test
     {
         static void main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!"); //Shortcut for Console.WriteLine is [cw + tab + tab], you write "cw" and double tap the tab key.
         }
     }
 }
