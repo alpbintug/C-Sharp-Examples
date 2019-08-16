75cc489ba516b346866e15a1cd64472a13f9e6b6
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-16 22:51:43 +0300

    Create 1-Hello-World.cs

---
+++ 1-Hello-World.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Sişarpdeneme
+{
+    class Test
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
