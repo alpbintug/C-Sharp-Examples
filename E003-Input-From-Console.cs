7fca40e6c621b9092443fd6afcbdea3c28380282
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-08-24 20:29:04 +0300

    Update E003-Input-From-Console.cs

--- E003-Input-From-Console.cs
+++ E003-Input-From-Console.cs
@@ -6,6 +6,7 @@ namespace CsharpExamples
     {
         static void Main(string[] args)
         {
+            /*Unlike in Java, you don't need to write multiple lines for a reader, but you have to handle the Exceptions. I didn't in this code because we haven't reached to that part yet.*/
             Console.WriteLine("Please enter a numerical value");
             string input1 = Console.ReadLine();
             Console.WriteLine("String value: "+input1);
@@ -16,6 +17,28 @@ namespace CsharpExamples
             double doubleInput1 = double.Parse(input1);
             Console.WriteLine("Parsed double value:" + doubleInput1);
 
+            /*
+            
+            For input 4325
+            
+            Output: 
+            Please enter a numerical value
+            4325
+            String value: 4325
+            Parsed integer value:4325
+            Converted integer value:4325
+            Parsed double value:4325
+            
+            For input Hello world!
+            
+            Output: 
+            
+            Please enter a numerical value
+            Hello world!
+            String value: Hello world!
+            System.FormatException: 'Input string was not in a correct format.'
+            
+            */
         }
     }
 }
