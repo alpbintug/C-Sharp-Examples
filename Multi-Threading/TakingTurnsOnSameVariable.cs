f9e2a367244209824a0f417af245b0a5a51bc02c
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2020-05-28 14:15:47 +0300

    Update and rename UsingSameVariable.cs to TakingTurnsOnSameVariable.cs

--- Multi-Threading/TakingTurnsOnSameVariable.cs
+++ Multi-Threading/TakingTurnsOnSameVariable.cs
@@ -3,14 +3,17 @@ using System.Threading;
 
 namespace Multi_thread
 {
-    class UsingSameVariable
+    class TakingTurnsOnSameVariable
     {
         static int x = 0;
+        //Variable to specify which thread is allowed to use the common variable X
         static bool turnToUseX = false;
         static void Main(string[] args)
         {
             Thread th1 = new Thread(new ThreadStart(increment));
             Thread th2 = new Thread(new ThreadStart(decrement));
+            //It does not matter which thread starts first, turnToUseX = false at first, which indicates thread 1 is permitted
+            //to use value of X and thread 2 will wait until thread 1 is done.
             th2.Start();
             th1.Start();
         }
@@ -18,12 +21,13 @@ namespace Multi_thread
         {
             for (int i = 0; i < 100; i++)
             {
+                //If turnTouseX = true, thread 2 is permitted to use it so thread 1 waits
                 while (turnToUseX)
                 {
 
                 }
-                x += 1;
-                Console.WriteLine($"incrementing x: {x}");
+                Console.WriteLine($"incrementing x: {++x}");
+                //When its job is done, thread 1 gives the turn to thread 2
                 turnToUseX = true;
             }
         }
@@ -31,12 +35,13 @@ namespace Multi_thread
         {
             for (int i = 0; i < 100; i++)
             {
+                //If turnTouseX = false, thread 1 is permitted to use it so thread 2 waits
                 while (!turnToUseX)
                 {
 
                 }
-                x -= 1;
-                Console.WriteLine($"decrementing x: {x}");
+                Console.WriteLine($"decrementing x: {--x}");
+                //When its job is done, thread 2 gives the turn to thread 1
                 turnToUseX = false;
             }
         }
